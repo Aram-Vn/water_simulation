dd0dc39d3b4cd7cf42c70f32abfb2c68923775dc
Aram-Vn <aramvardanyan13@gmail.com>
2024-09-13 17:00:53 +0400

    using clamp insted of if else

--- src/PIDController.cpp
+++ src/PIDController.cpp
@@ -1,56 +1,35 @@
 
 #include "../include/PIDController.h"
 
+#include <algorithm>
 #include <chrono>
 #include <cmath>
 #include <iostream>
 #include <thread>
 
 namespace my {
-    PIDController::PIDController(double kp, double ki, double kd, double minInputRate, double maxInputRate)
-        : m_kp{ kp },
-          m_ki{ ki },
-          m_kd{ kd },
-          m_previousError{},
-          m_integral{},
-          m_minInputRate{ minInputRate },
-          m_maxInputRate{ maxInputRate }
+PIDController::PIDController(double kp, double ki, double kd,
+                             double minInputRate, double maxInputRate)
+    : m_kp{kp}, m_ki{ki}, m_kd{kd}, m_previousError{}, m_integral{},
+      m_minInputRate{minInputRate}, m_maxInputRate{maxInputRate}
 
-    {
-    }
+{}
 
-    double PIDController::compute(double setpoint, double measured, double dt)
-    {
-        double error = setpoint - measured;
+double PIDController::compute(double setpoint, double measured, double dt) {
+  double error = setpoint - measured;
 
-        m_integral += error * dt * m_ki;
+  m_integral += error * dt * m_ki;
 
-        if (m_integral > m_maxInputRate)
-        {
-            m_integral = m_maxInputRate;
-        }
+  m_integral = std::clamp(m_integral, m_minInputRate, m_maxInputRate);
 
-        if (m_integral < m_minInputRate)
-        {
-            m_integral = m_minInputRate;
-        }
+  double derivative = (error - m_previousError) / dt;
 
-        double derivative = (error - m_previousError) / dt;
+  double output = error * m_kp + m_integral + derivative * m_kd;
 
-        double output = error * m_kp + m_integral + derivative * m_kd;
+  m_previousError = error;
 
-        m_previousError = error;
+  output = std::clamp(output, m_minInputRate, m_maxInputRate);
 
-        if (output > m_maxInputRate)
-        {
-            output = m_maxInputRate;
-        }
-
-        if (output < m_minInputRate)
-        {
-            output = m_minInputRate;
-        }
-
-        return output;
-    }
+  return output;
+}
 } // namespace my
